5fd3d3999006a9473fe75547a8ce50b4815af90d
AshutoshNema1 <125502706+AshutoshNema1@users.noreply.github.com>
2023-10-16 14:58:28 +0000

    learnt variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+// Problem Statement : You are building a login page and require details such as Email , Password, Customer Id , etc 
+
+const customerID = 548674
+console.log(customerID)
+
+let customerEmail = 'ashutoshnema419@gmail.com'
+var city = "Jabalpur"
+state = 'Madhya Pradesh'
+
+console.table([customerID,customerEmail,city,state])
+
+/* Prefer not to use var due to block scope and functional scope */
