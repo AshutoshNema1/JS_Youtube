b9be9f187d2b673a31654d93042b47f925be5574
AshutoshNema1 <125502706+AshutoshNema1@users.noreply.github.com>
2023-10-19 17:08:47 +0000

    used comparison operator b/w 0 null and undefined

---
+++ 01_basics/04_comaparisons.js
@@ -0,0 +1,23 @@
+// some basic comparisons are not mentioned here 
+
+// comaparison of 0 and null 
+
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+
+// the comaparison operator(> , <) converts the null value to zero that`s why the o/p of first consolelog gave false and last gave true.
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+// the case with undefined is not such it gives false on each of them
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined <= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
