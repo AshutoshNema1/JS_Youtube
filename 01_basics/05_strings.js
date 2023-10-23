59abc9573818019d468c67dca5fa8b831fa4da00
AshutoshNema1 <125502706+AshutoshNema1@users.noreply.github.com>
2023-10-23 17:47:54 +0000

    strings learnt

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+let name = "Ashutosh"
+
+let myName = "ashutosh"
+
+// Some str functions: 
+
+let myNameCapital = myName.toUpperCase()
+
+console.log(myNameCapital);
+
+// trim function in js:
+
+let spacedName = "   ashutosh   ";
+console.log(spacedName);
+console.log(spacedName.trim());
+
