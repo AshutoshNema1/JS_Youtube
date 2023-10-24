8fa655b864b012db8a8d7e161328e3ee0f68aa09
AshutoshNema1 <125502706+AshutoshNema1@users.noreply.github.com>
2023-10-24 12:44:28 +0000

    learnt dateTime module

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,20 @@
+let myDate = new Date();
+console.log(myDate);
+
+// console.log(myDate.toDateString());
+
+console.log(myDate.toISOString());
+
+console.log(typeof myDate.toISOString());
+
+console.log(typeof myDate.toJSON());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleTimeString());
+
+console.log(myDate.toTimeString())
+
+console.log(myDate.toUTCString())
+
+console.log(typeof myDate);
