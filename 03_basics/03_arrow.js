9ce875c27dd2a4550c41a5c3ce5ddb4eddea7721
AshutoshNema1 <125502706+AshutoshNema1@users.noreply.github.com>
2023-10-30 18:40:02 +0000

    learnt arrow funcs

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,28 @@
+// a normal function can be defined as follows 
+
+function greet(){
+    console.log("Hello World")
+}
+
+// an arrow function is declared as follows
+
+() => {} // This is an arrow func
+
+// Passing args to this arrow function
+
+// (num1,num2) => {
+//     return num1+num2
+// }
+
+// Saving it to some variable
+
+// let sum = (num1,num2) => {
+//     return num1+num2
+// }
+
+// A more short hand for this is if we want we can remove the return keyword we can write it like this
+
+// let sum = (num1, num2) => num1+num2
+// or
+// let sum = (num1, num2) => (num1+num2)
+console.log(sum(5,7))
